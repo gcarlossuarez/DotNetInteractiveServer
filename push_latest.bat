2daca0606c8ac0fa17c6a524683ad1a739dd2368
Germán Carlos Suárez Alanís <gcarlossuarez99@gmail.com>
2025-11-25 10:40:07 -0400

    Auto UPDATE: -11-2025_1040

---
+++ push_latest.bat
@@ -0,0 +1,14 @@
+@echo off
+REM Script para hacer push automático de los últimos cambios a GitHub
+
+git add .
+for /f "tokens=1-4 delims=/ " %%a in ("%date%") do set DATE=%%d-%%b-%%c
+for /f "tokens=1-2 delims=: " %%a in ("%time%") do set TIME=%%a%%b
+set MSG=Auto UPDATE: %DATE%_%TIME%
+git commit -m "%MSG%"
+git pull --rebase origin main
+git push origin main
+
+echo.
+echo Push completado. Presiona una tecla para salir.
+pause >nul
